813c6ae222ca5a3c86d7b0fd87d3a2b68e975ba2
agamlatiff <agam.latiff@gmail.com>
2025-11-08 15:47:47 +0700

    Add a lot of features

--- app/Livewire/Pages/DetailPage.php
+++ app/Livewire/Pages/DetailPage.php
@@ -2,35 +2,71 @@
 
 namespace App\Livewire\Pages;
 
-use App\Livewire\Traits\CategoryFilterTrait;
 use App\Models\Category;
 use App\Models\Foods;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
-class FavoritePage extends Component
+class DetailPage extends Component
 {
-    use CategoryFilterTrait;
-    
     public $categories;
-    public $selectedCategories = [];
-    public $items;
+    public $matchedCategory;
+    public $food;
     public $title = "Favorite";
 
-    public function mount(Foods $foods)
+    public function mount(Foods $foods, $id)
     {
         $this->categories = Category::all();
-        $this->items = $foods->getFavoriteFood();
+
+        $this->food = $foods->getFoodDetails($id)->first();
+
+        if (empty($this->food)) {
+            abort(404);
+        }
+
+        $this->matchedCategory = collect($this->categories)->firstWhere('id', $this->food->categories_id);
+    }
+    
+    public function addToCart()
+    {
+        $cartItems = session('cart_items', []);
+
+        $existingItemIndex = collect($cartItems)->search(fn($item) => $item['id'] === $this->food->id);
+
+        if ($existingItemIndex !== false) {
+            $cartItems[$existingItemIndex]['quantity'] += 1;
+        } else {
+            $cartItems[] = array_merge(
+                (array)$this->food,
+                [
+                    'quantity' => 1,
+                    'selected' => true,
+                ]
+            );
+        }
+    
+        session(['cart_items' => $cartItems]);
+        session(['has_unpaid_transaction' => false]);
+    
+        $this->dispatch('toast', 
+            data: [
+                'message1' => 'Item added to cart',
+                'message2' => $this->food->name,
+                'type' => 'success',
+            ]
+        );
     }
 
-    #[Layout('components.layouts.page')]
+    public function orderNow()
+    {
+        $this->addToCart();
+        return redirect()->route('payment.checkout');
+    }
+
+    #[Layout('components.layouts.app')]
     public function render()
     {
-        $filteredProducts = $this->getFilteredItems();
-
-        return view('product.favorite', [
-            'filteredProducts' => $filteredProducts,
-        ]);
+        return view('product.details');
     }
 }
